a614dccdbb3bdb50fc8ee278b3cfbae0c1479e27
certifiedloud <certifiedloud@gmail.com>
2015-05-05 09:53:11 -0600

    pep8 edits

--- clido.py
+++ clido.py
@@ -11,8 +11,8 @@ except ImportError:
     print("dopy is required. pip install dopy and try again")
 
 # Set the API token for authenticated interaction
-#IDEA cycle through a list of tokens to avoid rate limiting
-config_file= expanduser('~') + '/.clido.cfg'
+# IDEA cycle through a list of tokens to avoid rate limiting
+config_file = expanduser('~') + '/.clido.cfg'
 f = open(config_file, 'r')
 api_token = f.readline().split('=')[1]
 
@@ -25,9 +25,11 @@ Interact with your DigitalOcean account through their API.
 When run without any arguments, you simply get a list of
 all the active droplets in the specified account
 """
-parser = argparse.ArgumentParser(description = description)
-parser.add_argument('-s', '--show', help="Show details of droplet. Use the ID number of the droplet")
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('-s', '--show', help="Show details of droplet. Use the"
+                    "ID number of the droplet")
 args = parser.parse_args()
+
 
 # Show the details of a single droplet and exit
 if args.show:
