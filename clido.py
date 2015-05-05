37b582963094d1668a7016a2c72e654a4427fc53
certifiedloud <certifiedloud@gmail.com>
2015-05-05 06:11:04 -0600

    added config file support

--- clido.py
+++ clido.py
@@ -1,7 +1,8 @@
 import argparse
+from os.path import expanduser
 
 try:
-    import dopy
+    from dopy.manager import DoManager
 
 except ImportError:
     print("dopy is required. pip install dopy and try again")
@@ -9,8 +10,14 @@ except ImportError:
 # TODO Use argparse to set all of the CLI options
 parser = argparse.ArgumentParser(
     description = "Interact with your DigitalOcean account through their API")
-parser.add_argument
+#parser.add_argument
+parser.parse_args()
 
 # TODO set the API token for authenticated interaction
+config_file= expanduser('~') + '/.clido.cfg'
+f = open(config_file, 'r')
+api_token = f.readline().split('=')[1]
 
 # TODO run the command from dopy based on the given args
+do = DoManager(None, api_token, api_version=2)
+do.all_active_droplets()
