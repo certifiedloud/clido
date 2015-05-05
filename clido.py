fa5a9659e1526ef2cdf4fe5756b82ebe06cdad13
certifiedloud <certifiedloud@gmail.com>
2015-05-05 07:06:19 -0600

    added more options

--- clido.py
+++ clido.py
@@ -7,17 +7,27 @@ try:
 except ImportError:
     print("dopy is required. pip install dopy and try again")
 
-# TODO Use argparse to set all of the CLI options
-parser = argparse.ArgumentParser(
-    description = "Interact with your DigitalOcean account through their API")
-#parser.add_argument
-parser.parse_args()
-
-# TODO set the API token for authenticated interaction
+# set the API token for authenticated interaction
 config_file= expanduser('~') + '/.clido.cfg'
 f = open(config_file, 'r')
 api_token = f.readline().split('=')[1]
 
-# TODO run the command from dopy based on the given args
+# Initialize the API
 do = DoManager(None, api_token, api_version=2)
-do.all_active_droplets()
+
+# Use argparse to set all of the CLI options
+description = """
+Interact with your DigitalOcean account through their API.
+When run without any arguments, you simply get a list of
+all the active droplets in the specified account
+"""
+parser = argparse.ArgumentParser(description = description)
+parser.add_argument('-s', '--show', help="Show details of droplet. Use the ID number of the droplet")
+args = parser.parse_args()
+
+if args.show:
+    print(do.show_droplet(args.show))
+
+# run the command from dopy based on the given args
+for droplet in do.all_active_droplets():
+    print("{} {}".format(droplet['name'], droplet['id']))
