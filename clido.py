ff7f820fc38753f0ffcf775fba15281419cd84b4
certifiedloud <certifiedloud@gmail.com>
2015-05-05 08:41:22 -0600

    got the 'show' option to work

--- clido.py
+++ clido.py
@@ -1,5 +1,8 @@
 import argparse
+import json
+import sys
 from os.path import expanduser
+
 
 try:
     from dopy.manager import DoManager
@@ -7,7 +10,7 @@ try:
 except ImportError:
     print("dopy is required. pip install dopy and try again")
 
-# set the API token for authenticated interaction
+# Set the API token for authenticated interaction
 #IDEA cycle through a list of tokens to avoid rate limiting
 config_file= expanduser('~') + '/.clido.cfg'
 f = open(config_file, 'r')
@@ -26,9 +29,12 @@ parser = argparse.ArgumentParser(description = description)
 parser.add_argument('-s', '--show', help="Show details of droplet. Use the ID number of the droplet")
 args = parser.parse_args()
 
+# Show the details of a single droplet and exit
 if args.show:
-    print(do.show_droplet(args.show))
+    droplets = do.show_droplet(args.show)
+    print(json.dumps(droplets, sort_keys=True, indent=4))
+    sys.exit(0)
 
-# run the command from dopy based on the given args
+# If the program hasn't exited by now, just show a list of active droplets
 for droplet in do.all_active_droplets():
     print("{} {}".format(droplet['name'], droplet['id']))
