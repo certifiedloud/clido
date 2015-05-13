2f697cba2f2b2224a6ffa4dfc19df4ec22e3fdb3
certifiedloud <certifiedloud@gmail.com>
2015-05-13 15:41:46 -0600

    implemented domain creation

--- clido.py
+++ clido.py
@@ -25,15 +25,17 @@ all the active droplets in the specified account
 """
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('operand',
-                    help="specify one of the following: "\
+                    help="specify one of the following: "
                     "droplets, domains, images, sizes, regions, keys.")
-parser.add_argument('-c', '--create-droplet', action="store_true",
+parser.add_argument('-c', '--create', action="store_true",
                     help="create droplet. -c  -n <name> -s <size>"
                     "-i <image> -r <region>")
 parser.add_argument('-d', '--destroy',
                     help="destroy <operand> by <id>")
 parser.add_argument('-n', '--name',
                     help="name of droplet to create")
+parser.add_argument('-a', '--ip-address',
+                    help="Specify ip address for domain creation")
 parser.add_argument('-s', '--size',
                     help="size-slug of droplet to create")
 parser.add_argument('-i', '--image',
@@ -44,36 +46,29 @@ parser.add_argument('-k', '--ssh-keys', default=[], nargs='+', type=int,
                     help="list of ssh key id's to add to new droplets")
 args = parser.parse_args()
 
-# Check that all required args are present for droplet creation when -c is used
-if args.create_droplet:
-    if args.name is None or args.size is None or \
-            args.image is None or args.region is None:
-        parser.error("When using -c, you must specify -n <name>"
-                     "-s <size> -i <image> and -r <region>")
-
 if args.operand == 'sizes':
     sizes = do.get_all_sizes()
     for size in sizes:
         print(size)
 
-if args.operand == 'regions':
+elif args.operand == 'regions':
     # Print a detailed list of available regions
     regions = do.get_all_regions()
     for region in regions:
         print("{}({})".format(region, region.slug))
     sys.exit(0)
 
-if args.operand == 'keys':
+elif args.operand == 'keys':
     keys = do.get_all_sshkeys()
     for key in keys:
         print(key)
 
-if args.operand == 'images':
+elif args.operand == 'images':
     images = do.get_all_images()
     for image in images:
         print("{}({})".format(image, image.slug))
 
-if args.operand == 'droplets':
+elif args.operand == 'droplets':
     # check if we want to destroy a droplet
     if args.destroy:
         try:
@@ -88,12 +83,29 @@ if args.operand == 'droplets':
     for droplet in droplets:
         print(droplet)
 
-if args.operand == 'domains':
+elif args.operand == 'domains':
+    # check if we want to create a domain
+    if args.create:
+        if not args.name and not args.ip_address:
+            parser.error("In order to create a domain you must specify both "
+                         "--name and --ip-address")
+            sys.exit(1)
+        else:
+            try:
+                domain = digitalocean.Domain(token=api_token,
+                                             name=args.name,
+                                             ip_address=args.ip_address).create()
+                sys.exit(0)
+            except Exception as e:
+                print("Unable to create domain: {}".format(e))
+                sys.exit(1)
+
+    # If nothing else, just print a list of domains
     domains = do.get_all_domains()
     for domain in domains:
         print(domain)
 
 else:
     print("{} is not a valid operand, please choose one of the following:"
-            .format(args.operand))
+          .format(args.operand))
     print("droplets, domains, keys, regions, sizes, images.")
